0953ec0b4053ddabc2c23c9fc3d77680f53b1ca0
MrSmoke <syco711@gmail.com>
2021-06-17 12:02:03 +1000

    Add version page middleware

---
+++ src/AspNetCore/VersionPage/src/ApplicationInformation.cs
@@ -0,0 +1,8 @@
+namespace ClickView.GoodStuff.AspNetCore.VersionPage
+{
+    public class ApplicationInformation
+    {
+        public string ApplicationName { get; set; }
+        public string ApplicationVersion { get; set; }
+    }
+}
---
+++ src/AspNetCore/VersionPage/src/VersionPageResponseWriters.cs
@@ -0,0 +1,21 @@
+﻿namespace ClickView.GoodStuff.AspNetCore.VersionPage
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+
+    internal static class VersionPageResponseWriters
+    {
+        public static Task WriteMinimalPlaintext(HttpContext httpContext, ApplicationInformation information)
+        {
+            httpContext.Response.ContentType = "text/plain; charset=utf-8";
+
+            // https://www.ietf.org/rfc/rfc2046.txt (4.1.1)
+            // new line as crlf
+            const string newLine = "\r\n";
+
+            var response = information.ApplicationName + newLine + "Version: " + information.ApplicationVersion;
+
+            return httpContext.Response.WriteAsync(response);
+        }
+    }
+}
