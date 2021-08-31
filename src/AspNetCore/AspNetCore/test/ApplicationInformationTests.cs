18bcafe9f481139c9302ff4b9ec63d995884b1a8
MrSmoke <syco711@gmail.com>
2021-08-31 15:34:37 +1000

    add aspnetcore test project

---
+++ src/AspNetCore/AspNetCore/test/ApplicationInformationTests.cs
@@ -0,0 +1,14 @@
+﻿namespace ClickView.GoodStuff.AspNetCore.Tests
+{
+    using System;
+    using Xunit;
+
+    public class ApplicationInformationTests
+    {
+        [Fact]
+        public void FromAssembly_Null_ThrowsNullArgumentException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ApplicationInformation.FromAssembly(null!));
+        }
+    }
+}
