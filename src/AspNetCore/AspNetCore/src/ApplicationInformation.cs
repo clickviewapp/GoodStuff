c067a643dba3168bd26c2355e47745c1e933de5f
MrSmoke <syco711@gmail.com>
2021-08-31 15:27:34 +1000

    add methods to get ApplicationInformation from an assembly

--- src/AspNetCore/AspNetCore/src/ApplicationInformation.cs
+++ src/AspNetCore/AspNetCore/src/ApplicationInformation.cs
@@ -1,16 +1,54 @@
 namespace ClickView.GoodStuff.AspNetCore
 {
     using System;
+    using System.Diagnostics;
+    using System.Reflection;
 
     public sealed class ApplicationInformation
     {
         public ApplicationInformation(string name, string version)
         {
-            Name = name ?? throw new ArgumentNullException(nameof(name));
-            Version = version ?? throw new ArgumentNullException(nameof(version));
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Value cannot be empty", nameof(name));
+
+            if (string.IsNullOrWhiteSpace(version))
+                throw new ArgumentException("Value cannot be empty", nameof(version));
+
+            Name = name;
+            Version = version;
         }
 
         public string Name { get; }
         public string Version { get; }
+
+        public static ApplicationInformation FromAssembly(Assembly assembly)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException(nameof(assembly));
+
+            return FromFileVersion(FileVersionInfo.GetVersionInfo(assembly.Location));
+        }
+
+        public static ApplicationInformation FromFileVersion(FileVersionInfo fileVersionInfo)
+        {
+            if (fileVersionInfo == null)
+                throw new ArgumentNullException(nameof(fileVersionInfo));
+
+            if (string.IsNullOrWhiteSpace(fileVersionInfo.ProductName))
+            {
+                throw new ArgumentException(
+                    nameof(FileVersionInfo.ProductName) + " cannot be empty",
+                    nameof(fileVersionInfo));
+            }
+
+            if (string.IsNullOrWhiteSpace(fileVersionInfo.ProductVersion))
+            {
+                throw new ArgumentException(
+                    nameof(FileVersionInfo.ProductVersion) + " cannot be empty",
+                    nameof(fileVersionInfo));
+            }
+
+            return new ApplicationInformation(fileVersionInfo.ProductName, fileVersionInfo.ProductVersion);
+        }
     }
 }
