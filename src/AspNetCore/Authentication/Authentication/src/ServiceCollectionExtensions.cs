b9fe178d722969ce829d97b040a8f16d59f0e93e
Ryan <ryan.wang@clickview.com.au>
2021-09-01 11:22:34 +1000

    persist user session to redis

--- src/AspNetCore/Authentication/Authentication/src/ServiceCollectionExtensions.cs
+++ src/AspNetCore/Authentication/Authentication/src/ServiceCollectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace ClickView.GoodStuff.AspNetCore.Authentication
 {
-    using System;
+    using Abstractions;
     using Endpoints;
     using Infrastructure;
     using Microsoft.AspNetCore.Authentication;
@@ -8,6 +8,8 @@
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.DependencyInjection.Extensions;
     using Microsoft.Extensions.Options;
+    using StackExchangeRedis;
+    using System;
     using TokenValidation;
 
     public static class ServiceCollectionExtensions
@@ -45,6 +47,27 @@
 
             services.TryAddEnumerable(ServiceDescriptor
                 .Singleton<IPostConfigureOptions<TokenValidatorOptions>, TokenValidatorOptionsConfigureOptions>());
+        }
+
+        public static void AddRedisCacheUserSessionStore(this IServiceCollection services,
+            Action<RedisUserSessionCacheOptions> configure)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            if (configure == null)
+                throw new ArgumentNullException(nameof(configure));
+
+            services.AddOptions<RedisUserSessionCacheOptions>()
+                .Validate(o =>
+                {
+                    o.Validate();
+                    return true;
+                })
+                .Configure(configure);
+
+            //redis user session cache
+            services.AddSingleton<IUserSessionStore, RedisUserSessionStore>();
         }
     }
 }
---
+++ src/AspNetCore/Authentication/Authentication/src/StackExchangeRedis/RedisUserSessionCacheOptions.cs
@@ -0,0 +1,21 @@
+﻿namespace ClickView.GoodStuff.AspNetCore.Authentication.StackExchangeRedis
+{
+    using StackExchange.Redis;
+    using System;
+
+    public class RedisUserSessionCacheOptions
+    {
+        public IConnectionMultiplexer? Connection { get; set; }
+        public string? InstanceName { get; set; }
+        public TimeSpan? CacheExpiry { get; set; }
+
+        public void Validate()
+        {
+            if (Connection == null)
+                throw new ArgumentException("Redis Connection must be set", nameof(Connection));
+
+            if (string.IsNullOrWhiteSpace(InstanceName))
+                throw new ArgumentException("InstanceName must be set", nameof(InstanceName));
+        }
+    }
+}
