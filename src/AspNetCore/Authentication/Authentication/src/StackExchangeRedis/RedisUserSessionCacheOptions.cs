aa59e14fa768a5843e50cc807965bfa52a37a1d4
Ryan <ryan.wang@clickview.com.au>
2021-09-01 12:20:22 +1000

    added tests for redis user session store

--- src/AspNetCore/Authentication/Authentication/src/StackExchangeRedis/RedisUserSessionCacheOptions.cs
+++ src/AspNetCore/Authentication/Authentication/src/StackExchangeRedis/RedisUserSessionCacheOptions.cs
@@ -6,16 +6,12 @@
     public class RedisUserSessionCacheOptions
     {
         public IConnectionMultiplexer? Connection { get; set; }
-        public string? InstanceName { get; set; }
         public TimeSpan? CacheExpiry { get; set; }
 
         public void Validate()
         {
             if (Connection == null)
                 throw new ArgumentException("Redis Connection must be set", nameof(Connection));
-
-            if (string.IsNullOrWhiteSpace(InstanceName))
-                throw new ArgumentException("InstanceName must be set", nameof(InstanceName));
         }
     }
 }
