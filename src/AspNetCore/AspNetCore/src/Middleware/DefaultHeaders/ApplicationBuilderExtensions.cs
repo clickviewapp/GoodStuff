8c72bf9f5a7c06030f37e7ebfc374b916a9cc2e7
MrSmoke <syco711@gmail.com>
2021-11-09 18:46:25 +1100

    Add middleware which adds default headers to responses

---
+++ src/AspNetCore/AspNetCore/src/Middleware/DefaultHeaders/ApplicationBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace ClickView.GoodStuff.AspNetCore.Middleware.DefaultHeaders
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// Extension methods for adding default headers to the application
+    /// </summary>
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds middleware that adds default headers to the responses
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="options"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void UseDefaultHeaders(this IApplicationBuilder app,
+            DefaultHeadersMiddlewareOptions options)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            app.UseMiddleware<DefaultHeadersMiddleware>(Options.Create(options));
+        }
+    }
+}
---
+++ src/AspNetCore/AspNetCore/src/Middleware/DefaultHeaders/DefaultHeadersMiddleware.cs
@@ -0,0 +1,50 @@
+﻿namespace ClickView.GoodStuff.AspNetCore.Middleware.DefaultHeaders
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// Middleware to add a set of default headers specified in <see cref="DefaultHeadersMiddlewareOptions"/> to every response
+    /// </summary>
+    public class DefaultHeadersMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly DefaultHeadersMiddlewareOptions _options;
+
+        /// <summary>
+        /// Creates a new <see cref="DefaultHeadersMiddleware"/>
+        /// </summary>
+        /// <param name="next"></param>
+        /// <param name="options"></param>
+        public DefaultHeadersMiddleware(RequestDelegate next, IOptions<DefaultHeadersMiddlewareOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+
+        /// <summary>
+        /// Invokes the logic of the middleware
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public Task Invoke(HttpContext context)
+        {
+            if (_options.DefaultHeaders.Count == 0)
+                return _next(context);
+
+            context.Response.OnStarting(() =>
+            {
+                foreach (var (key, value) in _options.DefaultHeaders)
+                {
+                    context.Response.Headers.TryAdd(key, value);
+                }
+
+                return Task.CompletedTask;
+            });
+
+            return _next(context);
+        }
+    }
+}
