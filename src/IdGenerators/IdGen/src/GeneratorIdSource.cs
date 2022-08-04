d339ed31039be07cb51d1597a2271383e132e70d
MrSmoke <syco711@gmail.com>
2022-08-04 12:19:05 +1000

    Add IdLong generator

---
+++ src/IdGenerators/IdGen/src/GeneratorIdSource.cs
@@ -0,0 +1,88 @@
+﻿namespace ClickView.GoodStuff.IdGenerators.IdGen;
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+internal class GeneratorIdSource
+{
+    /// <summary>
+    /// Gets a generator id for use with IdGen
+    /// </summary>
+    /// <param name="bits">The number of bits the generated id should use</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="Exception"></exception>
+    public static int GetId(byte bits)
+    {
+        if (bits <= 0)
+            throw new ArgumentException("Value must be greater than 0", nameof(bits));
+
+        if(bits > 32)
+            throw new ArgumentException("Value must be less than 32", nameof(bits));
+
+        if (TryGetIdFromPrivateIpV4(bits, out var generatorId))
+            return generatorId;
+
+        throw new Exception("Failed to get Generator Id");
+    }
+
+    private static int GetMask(byte bits) => (1 << bits) - 1;
+
+    /// <summary>
+    /// Try and get a generator id from the local network interface
+    /// </summary>
+    /// <param name="bits"></param>
+    /// <param name="generatorId"></param>
+    /// <returns></returns>
+    private static bool TryGetIdFromPrivateIpV4(byte bits, out int generatorId)
+    {
+        var ipAddress = GetPrivateIpV4();
+
+        if (ipAddress == null)
+        {
+            generatorId = 0;
+            return false;
+        }
+
+        Trace.WriteLine($"Using IP Address {ipAddress} for Id Generator");
+
+        var bytes = ipAddress.GetAddressBytes();
+
+        // If we are on a LittleEndian system we need to swap the bits around
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(bytes);
+
+        var address = BitConverter.ToInt32(bytes, 0);
+
+        // Mask the value to get the correct length
+        var mask = GetMask(bits);
+        generatorId = address & mask;
+
+        return true;
+    }
+
+    internal static IPAddress? GetPrivateIpV4()
+    {
+        // Get all the ethernet (or wireless) interfaces
+        var interfaces = NetworkInterface.GetAllNetworkInterfaces()
+            .Where(x => x.NetworkInterfaceType is NetworkInterfaceType.Ethernet
+                            or NetworkInterfaceType.GigabitEthernet
+                            or NetworkInterfaceType.Wireless80211
+                            or NetworkInterfaceType.FastEthernetFx
+                            or NetworkInterfaceType.FastEthernetT
+                        && x.OperationalStatus == OperationalStatus.Up
+            );
+
+        // Get all the InterNetwork (IPv4) Unicast addresses
+        var addresses = interfaces.SelectMany(x => x.GetIPProperties().UnicastAddresses)
+            .Where(x => x.Address.AddressFamily == AddressFamily.InterNetwork)
+            .Select(x => x.Address);
+
+        // Get the first
+        return addresses.FirstOrDefault();
+    }
+}
---
+++ src/IdGenerators/IdGen/src/IdGenGenerator.cs
@@ -0,0 +1,37 @@
+﻿namespace ClickView.GoodStuff.IdGenerators.IdGen;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Abstractions;
+using global::IdGen;
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// An Id Generator that generates `<see cref="IdLong"/> ids
+/// </summary>
+public class IdGenGenerator : Abstractions.IIdGenerator<IdLong>
+{
+    private readonly IdGenerator _idGenerator;
+
+    /// <summary>
+    /// Creates a new instance of <see cref="IdGenGenerator"/>
+    /// </summary>
+    /// <param name="logger"></param>
+    public IdGenGenerator(ILogger<IdGenGenerator> logger)
+    {
+        var defaultOptions = new IdGeneratorOptions();
+        var generatorId = GeneratorIdSource.GetId(defaultOptions.IdStructure.GeneratorIdBits);
+
+        _idGenerator = new IdGenerator(generatorId, defaultOptions);
+
+        logger.LogDebug("GeneratorId: {GeneratorId}", _idGenerator.Id);
+    }
+
+    /// <inheritdoc />
+    public Task<IdLong> CreateAsync(CancellationToken token = default)
+    {
+        var id = _idGenerator.CreateId();
+
+        return Task.FromResult(new IdLong(id));
+    }
+}
