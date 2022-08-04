f6f6509c0c7420affc4fb039be330fa711b8e5d7
MrSmoke <syco711@gmail.com>
2022-08-04 12:45:22 +1000

    Add IdLong JsonConverter

---
+++ src/IdGenerators/Serialization/SystemTextJson/src/IdLongConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace ClickView.GoodStuff.IdGenerators.Serialization.SystemTextJson;
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Abstractions;
+
+/// <summary>
+/// JsonConverter for <see cref="IdLong"/>
+/// </summary>
+public class IdLongConverter : JsonConverter<IdLong>
+{
+    /// <inheritdoc />
+    public override IdLong Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return IdLong.Parse(reader.GetString()!);
+    }
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, IdLong value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
