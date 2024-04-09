02b5e94e49b2950e0acfb1a2cc12e0d23d599ec0
MrSmoke <709976+MrSmoke@users.noreply.github.com>
2024-04-09 12:04:58 +1000

    routing key cannot be null

--- src/Queues/RabbitMq/src/EnqueueOptions.cs
+++ src/Queues/RabbitMq/src/EnqueueOptions.cs
@@ -3,7 +3,7 @@ namespace ClickView.GoodStuff.Queues.RabbitMq;
 public class EnqueueOptions
 {
     public bool Persistent { get; init; }
-    public string? RoutingKey { get; init; }
+    public string RoutingKey { get; init; } = string.Empty;
 
     internal static readonly EnqueueOptions Default = new();
 }
