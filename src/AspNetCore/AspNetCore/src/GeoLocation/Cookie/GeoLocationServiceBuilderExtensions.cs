ad934f975e53f86888728850dcff1ee0e34b9e70
MrSmoke <709976+MrSmoke@users.noreply.github.com>
2024-04-19 15:45:52 +1000

    Fix AddCookie using the wrong types

--- src/AspNetCore/AspNetCore/src/GeoLocation/Cookie/GeoLocationServiceBuilderExtensions.cs
+++ src/AspNetCore/AspNetCore/src/GeoLocation/Cookie/GeoLocationServiceBuilderExtensions.cs
@@ -2,14 +2,13 @@ namespace ClickView.GoodStuff.AspNetCore.GeoLocation.Cookie;
 
 using System;
 using Microsoft.Extensions.DependencyInjection;
-using RequestHeader;
 
 public static class GeoLocationServiceBuilderExtensions
 {
     public static GeoLocationServiceBuilder AddCookie(this GeoLocationServiceBuilder builder,
-        Action<RequestHeaderGeoLocationProviderOptions>? configure = null)
+        Action<CookieGeoLocationProviderOptions>? configure = null)
     {
-        builder.Services.AddSingleton<IGeoLocationProvider, RequestHeaderGeoLocationProvider>();
+        builder.Services.AddSingleton<IGeoLocationProvider, CookieGeoLocationProvider>();
 
         if (configure is not null)
             builder.Services.Configure(configure);
