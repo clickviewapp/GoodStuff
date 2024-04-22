ee2a6a7f9e242625e9365edf3476817adcb0615b
MrSmoke <709976+MrSmoke@users.noreply.github.com>
2024-04-22 15:26:39 +1000

    Create base methods for QueueHostedService

--- src/Hosting/Queue/src/QueueHostedService.cs
+++ src/Hosting/Queue/src/QueueHostedService.cs
@@ -1,6 +1,5 @@
 namespace ClickView.GoodStuff.Hosting.Queue;
 
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Queues.RabbitMq;
@@ -11,122 +10,27 @@ using Queues.RabbitMq;
 /// </summary>
 /// <typeparam name="TMessage"></typeparam>
 /// <typeparam name="TOptions"></typeparam>
-public abstract class QueueHostedService<TMessage, TOptions> : IHostedService, IAsyncDisposable
+public abstract class QueueHostedService<TMessage, TOptions> : BaseQueueHostedService<TOptions>
     where TMessage : class, new()
     where TOptions : QueueHostedServiceOptions
 {
-    private readonly SemaphoreSlim _subscriptionLock = new(1, 1);
-    private readonly IQueueClient _queueClient;
-    private readonly string _name;
-    private SubscriptionContext? _subscriptionContext;
-    private bool _disposed;
-
-    /// <summary>
-    /// Gets the <see cref="ILogger"/>.
-    /// </summary>
-    protected readonly ILogger Logger;
-
-    /// <summary>
-    /// Gets the options.
-    /// </summary>
-    protected readonly TOptions Options;
-
     /// <summary>
     /// Initialises a new instance of <see cref="QueueHostedService{TMessage,TOptions}"/>.
     /// </summary>
     /// <param name="options"></param>
     /// <param name="loggerFactory"></param>
-    protected QueueHostedService(IOptions<TOptions> options, ILoggerFactory loggerFactory)
+    protected QueueHostedService(IOptions<TOptions> options, ILoggerFactory loggerFactory) : base(options, loggerFactory)
     {
-        var type = GetType();
-
-        _name = type.Name;
-        Options = options.Value;
-        Logger = loggerFactory.CreateLogger(type);
-
-        _queueClient = new RabbitMqClient(CreateOptions(Options, loggerFactory));
-    }
-
-    /// <summary>
-    /// Triggered when the application host is ready to start the service.
-    /// </summary>
-    /// <param name="cancellationToken"></param>
-    public async Task StartAsync(CancellationToken cancellationToken)
-    {
-        CheckDisposed();
-
-        await _subscriptionLock.WaitAsync(cancellationToken);
-
-        try
-        {
-            // If we have a context, don't start again
-            if (_subscriptionContext is not null)
-                throw new InvalidOperationException($"Cannot start already started service {_name} ");
-
-            var queueName = Options.QueueName;
-
-            if (string.IsNullOrWhiteSpace(queueName))
-                throw new ArgumentException($"{nameof(QueueHostedServiceOptions.QueueName)} is required");
-
-            _subscriptionContext = await _queueClient.SubscribeAsync<TMessage>(queueName,
-                OnMessageAsync,
-                new SubscribeOptions {PrefetchCount = Options.ConcurrentTaskCount},
-                cancellationToken);
-        }
-        finally
-        {
-            _subscriptionLock.Release();
-        }
-
-        Logger.LogInformation("Queue service started - {Name}", _name);
-    }
-
-    /// <summary>
-    /// Triggered when the application host is performing a graceful shutdown.
-    /// </summary>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    public async Task StopAsync(CancellationToken cancellationToken)
-    {
-        CheckDisposed();
-
-        await _subscriptionLock.WaitAsync(cancellationToken);
-
-        try
-        {
-            var subContext = _subscriptionContext;
-
-            if (subContext is not null)
-            {
-                await subContext.DisposeAsync();
-                _subscriptionContext = null;
-            }
-        }
-        finally
-        {
-            _subscriptionLock.Release();
-        }
-
-        Logger.LogInformation("Queue service stopped - {Name}", _name);
     }
 
     /// <inheritdoc />
-    public async ValueTask DisposeAsync()
+    protected override Task<SubscriptionContext> SubscribeAsync(IQueueClient queueClient, string queueName,
+        CancellationToken cancellationToken)
     {
-        if (_disposed)
-            return;
-
-        _disposed = true;
-
-        // If we still have an open subscription, Dispose it
-        if (_subscriptionContext != null)
-            await _subscriptionContext.DisposeAsync();
-
-        // Then finally dispose the client
-        await _queueClient.DisposeAsync();
-
-        // Clean up everything else
-        _subscriptionLock.Dispose();
+        return queueClient.SubscribeAsync<TMessage>(queueName,
+            OnMessageAsync,
+            new SubscribeOptions {PrefetchCount = Options.ConcurrentTaskCount},
+            cancellationToken);
     }
 
     /// <summary>
@@ -136,34 +40,6 @@ public abstract class QueueHostedService<TMessage, TOptions> : IHostedService, I
     /// <param name="token"></param>
     /// <returns></returns>
     protected abstract Task OnMessageAsync(TMessage message, CancellationToken token);
-
-    private static RabbitMqClientOptions CreateOptions(QueueHostedServiceOptions options, ILoggerFactory loggerFactory)
-    {
-        var host = options.Host;
-
-        if (string.IsNullOrWhiteSpace(host))
-            throw new ArgumentException($"{nameof(QueueHostedServiceOptions.Host)} is required");
-
-        var o = new RabbitMqClientOptions
-        {
-            Host = host,
-            Port = options.Port,
-            Username = options.Username,
-            Password = options.Password,
-            ConnectionTimeout = options.ConnectionTimeout,
-            EnableSsl = options.EnableSsl,
-            IgnoreSslErrors = options.IgnoreSslErrors,
-            LoggerFactory = loggerFactory
-        };
-
-        if (options.SslVersion != null)
-            o.SslVersion = options.SslVersion.Value;
-
-        if (options.Serializer != null)
-            o.Serializer = options.Serializer;
-
-        return o;
-    }
 
     /// <summary>
     /// Triggered when a message is received from the configured queue.
@@ -189,11 +65,5 @@ public abstract class QueueHostedService<TMessage, TOptions> : IHostedService, I
                 Logger.LogWarning("Cannot acknowledge task. Channel is not open");
             }
         }
-    }
-
-    private void CheckDisposed()
-    {
-        if (_disposed)
-            throw new ObjectDisposedException(GetType().Name);
     }
 }
--- src/Hosting/Queue/src/QueueHostedServiceOptions.cs
+++ src/Hosting/Queue/src/QueueHostedServiceOptions.cs
@@ -1,65 +1,12 @@
 namespace ClickView.GoodStuff.Hosting.Queue;
 
-using System.Security.Authentication;
-using Queues.RabbitMq.Serialization;
-
 /// <summary>
 /// Options for the <see cref="QueueHostedService{TMessage,TOptions}"/>
 /// </summary>
-public abstract class QueueHostedServiceOptions
+public abstract class QueueHostedServiceOptions : BaseQueueHostedServiceOptions
 {
-    /// <summary>
-    /// The name of the queue to subscribe to.
-    /// </summary>
-    public string? QueueName { get; set; }
-
-    /// <summary>
-    /// The hostname or network address of the queue server in which to connect.
-    /// </summary>
-    public string? Host { get; set; }
-
-    /// <summary>
-    /// The port that the queue server is listening to.
-    /// </summary>
-    public ushort Port { get; set; } = 5672;
-
-    /// <summary>
-    /// The username of the user for the queue.
-    /// </summary>
-    public string? Username { get; set; }
-
-    /// <summary>
-    /// The password of the user for the queue.
-    /// </summary>
-    public string? Password { get; set; }
-
-    /// <summary>
-    /// Timeout for connection attempts
-    /// </summary>
-    public TimeSpan? ConnectionTimeout { get; set; }
-
-    /// <summary>
-    /// Set to true to enable SSL
-    /// </summary>
-    public bool EnableSsl { get; set; }
-
-    /// <summary>
-    /// Set to false to ignore SSL errors
-    /// </summary>
-    public bool IgnoreSslErrors { get; set; }
-
-    /// <summary>
-    /// The TLS protocol version. Set to None to let the OS decide
-    /// </summary>
-    public SslProtocols? SslVersion { get; set; }
-
     /// <summary>
     /// The number of tasks to run concurrently.
     /// </summary>
     public ushort ConcurrentTaskCount { get; set; } = 1;
-
-    /// <summary>
-    /// The serializer to use for deserializing messages from the Queue
-    /// </summary>
-    public IMessageSerializer? Serializer { get; set; }
 }
